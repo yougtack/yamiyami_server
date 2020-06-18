ec85f002e2c6d6e8ddc6276f7718ebb1e1ba5cd1
parkhyunmin <hm.park@weddingbook.com>
2020-06-18 12:54:17 +0900

    vue index 수정.

--- vue/src/components/index.vue
+++ vue/src/components/index.vue
@@ -1,22 +1,34 @@
 <template>
-    <div class="index_img">
-      <div v-for="">
-        <router-link to="/SearchList"><img src="../assets/picture/1.png"/></router-link>
-        <router-link to="/SearchList"><img src="../assets/picture/2.png"/></router-link>
-        <router-link to="/SearchList"><img src="../assets/picture/3.png"/></router-link>
-        <router-link to="/SearchList"><img src="../assets/picture/4.png"/></router-link>
-      </div>
-      <div>
-        <router-link to="/SearchList"><img src="../assets/picture/5.png"/></router-link>
-        <router-link to="/SearchList"><img src="../assets/picture/6.png"/></router-link>
-        <router-link to="/SearchList"><img src="../assets/picture/7.png"/></router-link>
-        <router-link to="/SearchList"><img src="../assets/picture/8.png"/></router-link>
-      </div>
+  <div class="index_img">
+    <div v-for="">
+      <router-link to="/SearchList"><img src="../assets/picture/1.png"/></router-link>
+      <router-link to="/SearchList"><img src="../assets/picture/2.png"/></router-link>
+      <router-link to="/SearchList"><img src="../assets/picture/3.png"/></router-link>
+      <router-link to="/SearchList"><img src="../assets/picture/4.png"/></router-link>
     </div>
+    <div>
+      <router-link to="/SearchList"><img src="../assets/picture/5.png"/></router-link>
+      <router-link to="/SearchList"><img src="../assets/picture/6.png"/></router-link>
+      <router-link to="/SearchList"><img src="../assets/picture/7.png"/></router-link>
+      <router-link to="/SearchList"><img src="../assets/picture/8.png"/></router-link>
+    </div>
+    <div>
+      <router-link to="/SearchList"><img src="../assets/picture/9.png"/></router-link>
+      <router-link to="/SearchList"><img src="../assets/picture/10.png"/></router-link>
+      <router-link to="/SearchList"><img src="../assets/picture/11.png"/></router-link>
+      <router-link to="/SearchList"><img src="../assets/picture/12.png"/></router-link>
+    </div>
+    <div>
+      <router-link to="/SearchList"><img src="../assets/picture/13.png"/></router-link>
+      <img src="../assets/picture/none.png"/>
+      <img src="../assets/picture/none.png"/>
+      <img src="../assets/picture/none.png"/>
+    </div>
+  </div>
 </template>
 <script>
   export default {
-    date(){
+    date() {
 
     }
   }
